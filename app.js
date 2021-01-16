913349f110f12549c6bf9fd98b29212f98191578
infoboldsolutions <info@boldsolutions.hr>
2021-01-16 15:02:59 +0000

    posts router added by info

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const app = require('express')()
 const userRouter = require('./routes/users')
+const postsRouter = require('./routes/posts')
+const cors = require('cors')
+
+app.use(cors())
 
 app.use('/users', userRouter)
+app.use('/posts', postsRouter)
 
 app.listen(9999, '0.0.0.0')
---
+++ routes/posts.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const axios = require('axios').default
+
+
+
+router.get('/all', async (_, res) => {
+	let users_url = 'https://jsonplaceholder.typicode.com/posts'
+	try {
+		let { data } = await axios.get(users_url)
+		console.log('Fetching users')
+		return res.status(200).json(data)
+	} catch (e) {
+		console.log(e.message)
+		return res.status(400).json({ error: e.message })
+	}
+
+})
+
+module.exports = router
